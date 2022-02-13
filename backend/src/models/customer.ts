de7fefc2f42e53294633f2a24494deab67e8f1b4
Ruitao Li <ruital@andrew.cmu.edu>
2022-02-13 14:32:16 -0500

    issues still exists for testing, but interface added for customer

--- backend/src/models/customer.ts
+++ backend/src/models/customer.ts
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const customerSchema = new Schema(
+interface Customer {
+    username : string; 
+    password : string; // Salted, Hashed Password., 
+    full_name : string; 
+    addresses: Array<string>;
+}; 
+
+
+const customerSchema = new Schema<Customer>(
 {
     username : String, 
     password : String, // Salted, Hashed Password., 
@@ -10,4 +18,4 @@ const customerSchema = new Schema(
 });
 
 
-export default mongoose.model('Customer', customerSchema);
+export default mongoose.model<Customer>('Customer', customerSchema);
