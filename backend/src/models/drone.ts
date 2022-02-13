64647ea9eb2ffd9ed4c017b00635a818ca8c8b4c
Alex Chen <alexnchen@yahoo.com>
2022-02-13 16:45:10 -0500

    created interfaces for models

--- backend/src/models/drone.ts
+++ backend/src/models/drone.ts
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose';
-const { Schema } = mongoose;
+import { Schema, Types } from 'mongoose';
 
 interface Drone
 {
-   orders : Array<number>; // Reference to other ORDER objects
+   orders : Array<Types.ObjectId>; // Reference to other ORDER objects
    battery : number; // Out of 1.0
    status : string; // Could also be an enumerated type, with statuses ranging between “recharging”, “delivering”, “returning”, etc.
 };
    
 const droneSchema = new Schema(
 {
-   orders : [{object_id: Schema.Types.ObjectId }], // Reference to other ORDER objects
+   orders : [{object_id: Types.ObjectId, required : true, ref: 'Order'}], // Reference to other ORDER objects
    battery : Number, // Out of 1.0
    status : String // Could also be an enumerated type, with statuses ranging between “recharging”, “delivering”, “returning”, etc.
 });
