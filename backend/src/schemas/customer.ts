e98bc4c2b7683a75dda6c02f06fbd3d669f0b562
Alex Chen <alexnchen@yahoo.com>
2022-02-12 22:41:22 -0500

    schema for customer created, creating more schemas

---
+++ backend/src/schemas/customer.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const customerSchema = new Schema(
+{
+    username : String, 
+    password : String, // Salted, Hashed Password., 
+    full_name : String, 
+    addresses: [{ address: String }]
+});
