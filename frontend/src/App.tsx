f86dd395b130cf6affeb17c032455ae9300d1c86
AugustusSaalfeldCMU <asaalfel@andrew.cmu.edu>
2022-02-15 21:53:46 -0500

    Added temp routing code to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CustomNavbar from "./Components/CustomNavbar";
+import PendingOrderCard from "./Components/PendingOrders/PendingOrderCard";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+      <Routes>
+          <Route path="/pending" element= {<PendingOrderCard/>} />
+          <Route path="/" element= {<PendingOrderCard/>} />
+          <Route path="/checkout" element= {<PendingOrderCard/>} />
+      </Routes> 
     </div>
+    </Router>
   );
 }
 
