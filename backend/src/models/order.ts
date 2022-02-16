9a408703158307683ad1ef61b2e236294f04b0fe
Alex Chen <alexnchen@yahoo.com>
2022-02-15 22:59:28 -0500

    complete merge, two test failing at Rui Code

--- backend/src/models/order.ts
+++ backend/src/models/order.ts
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import mongoose, { Schema, Model, Document } from 'mongoose';
-
-interface donut_count {
-    donut_id: Schema.Types.ObjectId,
-    quantity: Number
-}
-
-export interface OrderInterface extends Document
-{
-    customer_id : Schema.Types.ObjectId; 
-=======
 import mongoose, { Schema, Model, Document, Types } from 'mongoose';
 
 
@@ -20,7 +8,6 @@ export interface donut_count {
 
 export interface OrderInterface extends Document{
     customer_id : Types.ObjectId; 
->>>>>>> 70425c1bc1eab84c199520c827a958d44e83f2ff
     donuts : Array<donut_count>;
     cost : number; 
     drone_id: Schema.Types.ObjectId;
@@ -36,22 +23,13 @@ const orderSchema = new Schema<OrderInterface>(
         {
             donut_id: Schema.Types.ObjectId, // String refers to donut objects
             quantity: Number// integer represents the quantity of donuts that is been ordered
-<<<<<<< HEAD
-        }],
-        cost : Schema.Types.Decimal128,
-=======
             }
         ],
         cost : Number,
->>>>>>> 70425c1bc1eab84c199520c827a958d44e83f2ff
         drone_id : Schema.Types.ObjectId,
         time_placed : Date,
         time_picked : Date,
         time_delivered : Date,
 });
 
-<<<<<<< HEAD
 export const OrderModel: Model<OrderInterface> = mongoose.model<OrderInterface>('Order', orderSchema);
-=======
-export const OrderModel: Model<OrderInterface> = mongoose.model<OrderInterface>('Order', orderSchema);
->>>>>>> 70425c1bc1eab84c199520c827a958d44e83f2ff
