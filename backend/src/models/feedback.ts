f06a5cdfabb1a3c659cdb728a0b9c6a9e8bc96ab
Alex Chen <alexnchen@yahoo.com>
2022-02-15 23:08:55 -0500

    complete merge, two test failing at Rui Code stil

--- backend/src/models/feedback.ts
+++ backend/src/models/feedback.ts
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-import mongoose, { Schema, Model, Document } from 'mongoose';
-
-export interface FeedbackInterface extends Document
-{
-    customer_id : Schema.Types.ObjectId; 
-    order_id : Schema.Types.ObjectId;
-=======
 import mongoose, { Schema, Model, Document, Types } from 'mongoose';
 
 export interface FeedbackInterface extends Document {
     customer_id : Types.ObjectId; 
     order_id : Types.ObjectId;
->>>>>>> 70425c1bc1eab84c199520c827a958d44e83f2ff
     rating : number; 
     feedback: string;
 }; 
