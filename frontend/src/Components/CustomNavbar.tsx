0b3ada31746577940424f34a9e9fd1c5a77dda4d
Nithin Ravi <nravi@andrew.cmu.edu>
2022-02-28 16:50:24 -0500

    Added permissions lite to home page routing and drone status page

--- frontend/src/Components/CustomNavbar.tsx
+++ frontend/src/Components/CustomNavbar.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import {Container, Navbar, Nav} from 'react-bootstrap'
 import logo from '../image/favicon-32x32.png';
 
+interface Props {
+  permissions: string;
+}
 
-function CustomNavbar() {
+
+function CustomNavbar(props: Props) {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
@@ -13,8 +17,8 @@ function CustomNavbar() {
         </Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/">Menu</Nav.Link>
-          <Nav.Link href="/pending">Pending Orders</Nav.Link>
-          <Nav.Link href="/checkout">Checkout</Nav.Link>
+          {(props.permissions == "Employee") ? <Nav.Link href="/pending">Pending Orders</Nav.Link> : <Nav.Link href="/checkout">Checkout</Nav.Link>}
+          <Nav.Link href="/delivery_status">Delivery Status</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
