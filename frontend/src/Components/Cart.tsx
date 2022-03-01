d928bdfac651efb5ec135dee2f46154fd3144e64
Nithin Ravi <nravi@andrew.cmu.edu>
2022-03-01 18:33:32 -0500

    halfway there?

--- frontend/src/Components/Cart.tsx
+++ frontend/src/Components/Cart.tsx
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Grid, Card, Button, Text, Spacer} from '@geist-ui/core'
-import { donutData } from '../data/dummydata';
+// import { donutData } from '../data/dummydata';
 
 
 
 
 function Cart() {
+  const [orderData, setOrderData] = useState<Array<any>>([]);
+  const [donutData, setDonutData] = useState<Array<any>>([]);
+
+
+  const fetchOrderData = async () => {
+    const response = await fetch('/orders').then(response => response.json())
+   .then(result => {console.log('orders', result); setOrderData(result)});
+    return response;
+ };
+  const fetchDonutData = async () => {
+  //   const response = await fetch('/donuts').then(response => response.json())
+  //  .then(result => {console.log('hello', result); setDonutData(result)});
+  //   return response;
+    console.log("penis");
+    console.log(orderData[0]);
+    setDonutData(orderData[0]);
+  };
+
+  const updateDatabaseQuantity = (order: string, value: string) => {
+    const currentOrder = orderData[0];
+    const currentOrderId = orderData[0]._id;
+    const num = parseInt(value)
+    //iterate quantity using post operation
+    var raw = JSON.stringify({"donut_id":"621e88c90db3439bca66cbf2","quantity":num});
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    fetch("http://localhost:3001/orders/add_item/" + currentOrderId, {
+      method: 'PUT',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    })
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+    
+  }
+  useEffect(() => { 
+    fetchOrderData();
+    fetchDonutData();
+    }, []);
   return (
     <div>
       <Grid.Container gap={1} justify="center" height="300px" width="100%">
+        
+      {/* onClick={() => updateDatabaseQuantity(1)} */}
       {donutData.map((data, key) => {
           return (
             <div key={key}>
--- frontend/src/Components/Menu.tsx
+++ frontend/src/Components/Menu.tsx
@@ -2,24 +2,60 @@ import React, { useState, useEffect } from 'react';
 import {Grid, Card, Button, Text, Spacer, Input} from '@geist-ui/core'
 // import { donutData } from '../data/dummydata';
 import { FormControl } from 'react-bootstrap';
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
 
 function Menu() {
-  const [donutData, setData] = useState<Array<any>>([]);
-   const fetchData = async () => {
+  const [donutData, setDonutData] = useState<Array<any>>([]);
+  const [orderData, setOrderData] = useState<Array<any>>([]);
+
+   const fetchDonutData = async () => {
      const response = await fetch('/donuts').then(response => response.json())
-    .then(result => {console.log('hello', result); setData(result)});
+    .then(result => {console.log('hello', result); setDonutData(result)});
      return response;
   };
+  const fetchOrderData = async () => {
+    const response = await fetch('/orders').then(response => response.json())
+   .then(result => {console.log('orders', result); setOrderData(result)});
+    return response;
+ };
+ 
   useEffect(() => {
-    fetchData();
+    fetchDonutData();
+    fetchOrderData();
     }, []);
   const [quantity, setQuantity] = useState<number>(0);
   return (
     <div>
       <Grid.Container gap={1} justify="center" height="100px">
       {donutData.map((data, key) => {
+
         const updateQuantity = (value: string) => {
           const num = parseInt(value)
+          if (isNaN(num) || num < 0) {
+            setQuantity(0);
+          }
+          else {
+            setQuantity(num);
+          }
+        }
+        const updateDatabaseQuantity = (order: string, value: string) => {
+          const currentOrder = orderData[0];
+          const currentOrderId = orderData[0]._id;
+          const num = parseInt(value)
+          //iterate quantity using post operation
+          var raw = JSON.stringify({"donut_id":"621e88c90db3439bca66cbf2","quantity":num});
+
+          fetch("http://localhost:3001/orders/add_item/" + currentOrderId, {
+            method: 'PUT',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+          })
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+          
           if (isNaN(num) || num < 0) {
             setQuantity(0);
           }
@@ -41,11 +77,11 @@ function Menu() {
                   <Text p b>
                     Price: {data.price}
                   </Text>
-                  <Grid.Container gap={2} height="100px" justify="center">
+                  {/* <Grid.Container gap={2} height="100px" justify="center">
                     <Grid><Button auto type="secondary" onClick={() => updateQuantity((quantity-1).toString())}>-</Button></Grid>
                     <Grid><FormControl width="50px" value={quantity} onChange={(event) => updateQuantity(event.target.value)}/></Grid>
                     <Grid><Button auto type="secondary"onClick={() => updateQuantity((quantity+1).toString())}>+</Button></Grid>
-                  </Grid.Container>
+                  </Grid.Container> */}
                   <Button auto type="success">Add to Cart</Button>
                 </Card>
                 <Spacer h={2}/>
