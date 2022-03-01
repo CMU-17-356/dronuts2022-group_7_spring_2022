ef321d014e21b3e9498537ab662c0f562e497d18
Jas Haria <37465756+jas-haria@users.noreply.github.com>
2022-03-01 15:18:25 +0000

    changing cart screen

--- frontend/src/Components/Cart.tsx
+++ frontend/src/Components/Cart.tsx
@@ -13,7 +13,7 @@ function Cart() {
           return (
             <div key={key}>
                 <Grid md={24}>
-                <Card shadow width="100%" >
+                <Card shadow width="800px" >
                   {/* <img src={require(`${data.image}`)} alt="Donut Pic" /> */}
                   <Text p b>
                     {data.name}
@@ -32,21 +32,15 @@ function Cart() {
             </div>
           );
         })}
+        <Grid xs={12} justify="center"></Grid>
+        <Card>
+          <Text h4 my={0}>Total: $10.35</Text>
+          <Card.Footer>
+            <Button auto type="success">Place Order</Button>
+          </Card.Footer>
+        </Card>
     </Grid.Container>
-    <Grid.Container gap={1.5}>
-      <Grid xs={12} justify="center"></Grid>
-      <Card>
-        <Text h4 my={0}>Total: $10.35</Text>
-        <Card.Footer>
-          <Button auto type="success">Place Order</Button>
-        </Card.Footer>
-      </Card>
-    </Grid.Container>
-        
-    </div>
-
-
-    
+    </div>   
   );
 }
 
