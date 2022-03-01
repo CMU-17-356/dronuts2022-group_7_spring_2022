ce1ff9975d218b3fdb833dc926f61455388edf23
Ruitao Li <ruitaol@andrew.cmu.edu>
2022-02-28 20:04:23 -0500

    controller written for donuts

---
+++ backend/src/donut-controllers.ts
@@ -0,0 +1,40 @@
+import * as mongoose from "mongoose";
+import { Request, Response } from "express";
+import { DonutModel, DonutInterface } from './models/donut';
+
+export let listAllDonut = (req: Request, res: Response) => {
+    let donut = DonutModel.find({}, (err: any, result: any) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(result);
+      }
+    });
+};
+
+
+export let upsertDonut = (req: Request, res: Response) => {
+    let filter = { _id:req.params.id};
+    let update = req.body;
+    let donut = DonutModel.findOneAndUpdate(filter, update, {new: true}, (err: any, result: any) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(result);
+      }
+    });
+};
+
+export let deleteDonutById = (req: Request, res: Response) => {
+    let donut = DonutModel.deleteOne({ _id: req.params.id }, (err: any) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("Successfully Deleted Book");
+      }
+    });
+};
+
+
+
+
