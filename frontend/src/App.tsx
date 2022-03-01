8735c61544b25d8086619b1e087a97d62390fceb
AugustusSaalfeldCMU <asaalfel@andrew.cmu.edu>
2022-03-01 13:26:32 -0500

    can easily switch between employee and customer view w button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import CustomNavbar from "./Components/CustomNavbar";
 import PendingOrderCard from "./Components/PendingOrders/PendingOrderCard";
 import Menu from "./Components/Menu";
+import {Button} from '@geist-ui/core'
 import Cart from "./Components/Cart";
-
-
 
 import {
   BrowserRouter as Router,
@@ -14,15 +13,27 @@ import {
 } from "react-router-dom";
 import DeliveryStatus from './Components/DeliveryStatus';
 
-var permissions = ""
+const customer_string = "Customer";
+const employee_string = "Employee";
+var permissions = customer_string
 
 function App() {
+
+  const [currPermission, setPermission] = useState(permissions);
+
   return (
     <Router>
       <div>
-        <CustomNavbar permissions = {permissions}/>
+        <CustomNavbar permissions = {currPermission}/>
+        <Button auto type="secondary" onClick={() => {
+        if (currPermission === employee_string) {
+          setPermission(customer_string);
+        } else { 
+          setPermission(employee_string);
+        }
+      }}>Switch to {currPermission === employee_string ? employee_string : customer_string} view</Button>
       <Routes>
-          {permissions == "Employee" ? <Route path="/pending" element= {<PendingOrderCard/>} /> : <Route path="/checkout" element= {<Cart/>} />}
+          {currPermission === employee_string ? <Route path="/pending" element= {<PendingOrderCard/>} /> : <Route path="/checkout" element= {<Cart/>} />}
           <Route path="/" element= {<Menu/>} />
           <Route path="/delivery_status" element= {<DeliveryStatus/>} />
       </Routes> 
