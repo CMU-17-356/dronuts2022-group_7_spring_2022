763422f4f3f4d9826ed9357ff624b2bb0bcf95a7
Alex Chen <alexnchen@yahoo.com>
2022-03-01 12:51:32 -0500

    mostly done with order controllers, need some testing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import { DonutModel, DonutInterface } from './models/donut';
 import * as donut_controllers from './controllers/donut_controllers';
+import * as order_controllers from './controllers/order_controllers';
 
 const app = express();
 const port = 3001;
@@ -10,8 +11,8 @@ const port = 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//donut CRUD methods
-app.get('/donuts', donut_controllers.listAllDonut
+// Donut Controllers
+app.get('/donuts', donut_controllers.listAllDonuts
 );
 
 app.get('/donuts/:id', donut_controllers.getDonutById
@@ -32,13 +33,40 @@ app.delete('/donuts/:id', donut_controllers.deleteDonutById
 app.delete('/donuts', donut_controllers.deleteDonutByName
 );
 
-// Donut Controllers
-
 // Customer Controllers
 
 // Employee Controllers
 
 // Orders
+app.get('/orders', order_controllers.listAllOrders
+);
+
+app.get('/orders/incomplete', order_controllers.listIncompleteOrders
+);
+
+app.get('/orders/pending', order_controllers.listPendingOrders
+);
+
+app.get('/orders/past', order_controllers.listPastOrders
+);
+
+app.put('/orders/', order_controllers.createOrder
+);
+
+app.post('/orders/', order_controllers.createOrder
+);
+
+app.put('/orders/:id', order_controllers.upsertOrderById
+);
+
+app.post('/orders/:id', order_controllers.upsertOrderById
+);
+
+app.post('/orders/:id', order_controllers.upsertOrderById
+);
+
+app.delete('/orders/:id', order_controllers.deleteOrderById
+);
 
 app.listen(port, () => {
   console.log('Dronuts-App listening on localhost:{port}');
