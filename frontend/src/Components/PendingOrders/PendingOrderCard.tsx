8ee238e1ab9b634257e264fd1b79aaf43eef8998
AugustusSaalfeldCMU <asaalfel@andrew.cmu.edu>
2022-03-21 11:52:38 -0400

    Orders page now converts donut id to donut name

--- frontend/src/Components/PendingOrders/PendingOrderCard.tsx
+++ frontend/src/Components/PendingOrders/PendingOrderCard.tsx
@@ -6,17 +6,14 @@ import './PendingOrderCard.css';
 import PastOrderCard from './PastOrderCard';
 
 import axios from 'axios';
+import { resourceLimits } from 'worker_threads';
+import { set } from 'mongoose';
 
 function PendingOrderCard() {
-  const [orderData, setOrderData] = useState<Array<any>>([]);
+  const [donutDict, setDonutMap] = useState<[]>([]);
   const [pastOrderData, setPastOrderData] = useState<Array<any>>([]);
   const [pendingOrderData, setPendingOrderData] = useState<Array<any>>([]);
 
-  const fetchAllOrders = async () => {
-    const response = await fetch('/orders').then(response => response.json())
-    .then(result => {console.log('order', result); setOrderData(result)});
-    return response;
-  }
   const fetchPendingOrderData = async () => {
     const response = await fetch('/orders/pending').then(response => response.json())
     .then(result => {console.log('pending', result); setPendingOrderData(result)});
@@ -27,16 +24,23 @@ function PendingOrderCard() {
     .then(result => {console.log('past', result); setPastOrderData(result)});
     return response;
   }
+  const fetchAllDonuts = async () => {
+    const response = await fetch('/donuts').then(response => response.json())
+    .then(result => {console.log('past', result); 
+    let dictionary = Object.assign({}, ...result.map((v: any) => ({[v._id]: v.name})));
+    setDonutMap(dictionary)});
+    return response;
+  }
   
   useEffect(() => { 
-    fetchAllOrders();
+    fetchAllDonuts();
     fetchPendingOrderData();
     fetchPastOrderData();
     }, []);
   
   return (
     
-    <div className="pending-orders-page">
+    <div className="orders-page">
       <Card shadow width="100%">
         <Text h4>Pending Orders</Text>
         <Divider />
@@ -45,14 +49,14 @@ function PendingOrderCard() {
            return(<div key={key}>
             <Card shadow>
               <Text h5 className="order-card-title">Order #{data._id} — Placed at: {data.time_placed}.</Text>
-              <Text h6 className="order-card-drone mb-2 text-muted">Drone #{data.drone_id} status — battery</Text>
+              <Text h6 className="order-card-drone mb-2 text-muted">Drone #{data.drone_id}</Text>
               <Text h6 className="order-card-drone mb-2 text-muted">Cost: ${data.cost}</Text>
               <Divider />
               <div className="order-body">
                 <ul>
                 {data.donuts.map((donutData: any, donutKey: any) => {
                     return(
-                      <li key={donutKey}><Text>Donut {donutData.donut_id} - {donutData.quantity}</Text></li>);})}
+                      <li key={donutKey}><Text>{donutDict[donutData.donut_id]} - {donutData.quantity}</Text></li>);})}
                 </ul>
               </div>
             </Card>
@@ -72,7 +76,7 @@ function PendingOrderCard() {
                     <Text>Order #{pastOrderData._id} -- Time Completed: {pastOrderData.time_delivered} </Text>
                       <ul>
                         {pastOrderData.donuts.map((pastDonutData: any, donutKey: any) => {
-                          return(<li key={donutKey}><Text>Donut {pastDonutData.donut_id} - {pastDonutData.quantity}</Text></li>);
+                          return(<li key={donutKey}><Text>{donutDict[pastDonutData.donut_id]} - {pastDonutData.quantity}</Text></li>);
                         })}
                       </ul>
                     </Card>
