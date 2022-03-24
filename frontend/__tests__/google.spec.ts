3d50b037996b0a2f0e01cf1a47dc956b408bf97e
Nithin Ravi <nravi@andrew.cmu.edu>
2022-03-24 17:16:50 -0400

    Finished e2e test with correct google example

--- frontend/__tests__/google.spec.ts
+++ frontend/__tests__/google.spec.ts
@@ -1,9 +1,23 @@
 export {}
+const puppeteer = require('puppeteer');
 
-describe("Google.com", () => {
-    it('should get correct title when go to home page', async () => {
-      await page.goto('https://google.com');
-      const title = await page.title();
-      expect(title).toEqual('Google');
+describe("https://dronutgroup7frontend.ue.r.appspot.com", () => {
+    it('should have correct donut data displayed on home page', async () => {
+      await page.goto('https://dronutgroup7frontend.ue.r.appspot.com');
+      // const name = await page.$eval('.hnname > a', el => el.innerText)
+      // console.log(name)
+      // if ((await page.waitForXPath('//*[contains(text(), "Subscription Confirmed")]',30000)) !== null) {
+      //   chk = await page.evaluate(el => el.innerText, await page.$x('//*[contains(text(), "Subscription Confirmed")]'))
+      //   chk = 'Success'
+      // } else { 
+      //   chk = 'Failed'
+      // }     
+      const found = (await page.content()).match(/Strawberry/g);
+      // const found = await page.evaluate(() => window.find("your_text"));
+      // const title = await page.title();
+      // expect(title).toEqual('Google');
+      console.log("found value:", found);
+      expect(found).not.toBeNull();
+      // expect(await page.evaluate(() => window.find("your_text"));
     });
   });
