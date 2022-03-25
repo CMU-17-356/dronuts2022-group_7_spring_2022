a85a75e966574f3df3fa8bdff689c9468f31887b
AugustusSaalfeldCMU <asaalfel@andrew.cmu.edu>
2022-03-24 21:48:31 -0400

    modifying employee order

--- frontend/src/Components/PendingOrders/ModifyEmployeeOrder.tsx
+++ frontend/src/Components/PendingOrders/ModifyEmployeeOrder.tsx
@@ -57,7 +57,44 @@ function ModifyEmployeeOrderCard() {
     console.log(handleSubmit);
   }  
 
-  const removeDonut  = (value: string) => {
+  const removeDonut  = async (donutID: string, quantity: number) => {
+      var newDonutsList: any[] = [];
+      var removed = false;
+      var newDIdx = 0;
+      
+      curr_order.donuts.forEach((currD: any) => {
+        if (!removed && currD.donut_id == donutID && quantity == currD.quantity) {
+          removed = true;
+        } else {
+          newDonutsList[newDIdx] = currD;
+          newDIdx++;
+        }
+      }); 
+
+      let raw = JSON.stringify(newDonutsList)
+      await fetch('https://dronutsgroup7backend.uk.r.appspot.com/orders/add_donuts/'+curr_order._id, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: raw
+    });
+
+      /*var removed = false;
+      var newDIdx = 0;
+      for (var i = 0; i < curr_order.donuts.length; i++) {
+        let currD = curr_order.donuts[i];
+
+          if (!removed && currD.donut_id == donutID && quantity == currD.quantity) {
+            removed = true;
+          } else {
+            newDonuts[newDIdx] = currD;
+            newDIdx++;
+          }
+      }
+
+      curr_order.donuts = newDonuts;
+      console.log("new length: " + curr_order.donuts.length);*/
   }
 
   const changeSelectedDonut = (selectedOption: any) => {
@@ -84,9 +121,9 @@ function ModifyEmployeeOrderCard() {
     //Check if curr_order has donut, if so add one more donut
     //If not, append new donut to curr_order list.
     var containsDonut = false;
-    curr_order.donuts.map((donut: any, donutKey: any) => {
-      if(!containsDonut && donutKey == selectedDonut) {
-        updateQuantity(donut.quantity, 1);
+    curr_order.donuts.map((donut: any) => {
+      if(!containsDonut && donut.donut_id == selectedDonut) {
+        updateQuantity(donut.quantity, 1, -1);
         containsDonut = true;
       }
     });
@@ -94,12 +131,32 @@ function ModifyEmployeeOrderCard() {
     if (!containsDonut) curr_order.donuts.push({'donut_id': selectedDonut.value, 'quantity':1, '_id': new_id});
   }
   
-  const updateQuantity = (donut: any, modifier: number) => {
-    var num = donut.quantity + modifier;
-    if (isNaN(num) || num < 0) {
-        num = 0;
+  const updateQuantity = (donut: any, modifier: number, formIdx: number) => {
+    console.log(modifier);
+    var oldQ = donut.quantity;
+    var newQ = donut.quantity + modifier;
+    if (isNaN(newQ) || newQ < 0) {
+      newQ = 0;
     }
-    donut.quantity = num;
+    donut.quantity = newQ;
+
+    if (formIdx != -1) {
+      for (var i = 0; i < curr_order.donuts.length; i++) {
+        let currD = curr_order.donuts[i];
+        console.log(currD);
+        console.log("i:" + i);
+        if (currD.donut_id == donut.donut_id && oldQ == currD.quantity) {
+          console.log("found!");
+          currD.quantity = newQ;
+          
+          console.log((document.getElementById("donut"+formIdx.toString()) as any).value);
+          (document.getElementById("donut"+formIdx.toString()) as any).value = currD.quantity;
+          console.log("found element, new order: " + JSON.stringify(curr_order));
+          return;
+        }
+      }
+    } 
+
     console.log(JSON.stringify(curr_order));
   }
   
@@ -126,14 +183,15 @@ function ModifyEmployeeOrderCard() {
                 <Text h6 className="order-card-drone mb-2 text-muted">Cost: ${curr_order.cost}</Text>
                 <Divider />
                 <ul>
-                {curr_order.donuts.map((donut: any, donutKey: any) => {
-                    return(<li key={donutKey}>
+                {curr_order.donuts.map((donut: any, formIdx: any) => {
+                    return(<li>
                         <Grid.Container gap={1} justify="center" height="300px" width="100%">
                           <Grid><Text>{donutDict[donut.donut_id]}</Text></Grid>
-                          <Grid><Button auto type="secondary" onClick={() => updateQuantity(donut, -1)}>-</Button></Grid>
-                          <Grid><FormControl width="50px" value={donut.quantity} onChange={(event) => updateQuantity(donut, (event.target as any).value)}/></Grid>
-                          <Grid><Button auto type="secondary"onClick={() => updateQuantity(donut, +1)}>+</Button></Grid>
-                          <Button auto type="error" onClick={() => removeDonut(donut.donut_id)}>Remove</Button>
+                          <Grid><Button auto type="secondary" onClick={() => updateQuantity(donut, -1, formIdx)}>-</Button></Grid>
+                          {console.log(formIdx)}
+                          <Grid><FormControl type="text" width="50px" id={"donut"+formIdx} value={donut.quantity} onChange={(event) => updateQuantity(donut, (event.target as any).value, formIdx)}/></Grid>
+                          <Grid><Button auto type="secondary"onClick={() => updateQuantity(donut, 1, formIdx)}>+</Button></Grid>
+                          <Button auto type="error" onClick={() => removeDonut(donut.donut_id, donut.quantity)}>Remove</Button>
                         </Grid.Container>
                         </li>);
                 })}
