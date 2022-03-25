5f68486978c37cceb66cde7f992666998041318b
Nithin Ravi <nravi@andrew.cmu.edu>
2022-03-24 22:54:33 -0400

    Completed e2e testing

--- frontend/__tests__/employee.spec.ts
+++ frontend/__tests__/employee.spec.ts
@@ -5,9 +5,7 @@ const puppeteer = require('puppeteer');
 describe("https://dronutgroup7frontend.ue.r.appspot.com/pending", () => {
     it('Check whether the Employee loading page render', async () => {   
       await page.goto('https://dronutgroup7frontend.ue.r.appspot.com/pending'); 
-      const found = await (await page.content()).match(/pending/gi);
-      console.log(await page.content());
-      console.log("found value:", found);
+      const found = await (await page.content()).match(/loading/gi);
       await expect(found).not.toBeNull();
     });
   });
